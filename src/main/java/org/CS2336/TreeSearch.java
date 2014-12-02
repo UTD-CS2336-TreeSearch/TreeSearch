6ce767ab4af6b065a62306e38374765b40447fb9
Michael Aldridge <aldridge.mac@gmail.com>
2014-12-02 15:48:01 -0600

    added machine parsable mode

--- src/main/java/org/CS2336/TreeSearch.java
+++ src/main/java/org/CS2336/TreeSearch.java
@@ -3,12 +3,24 @@ package org.CS2336;
 import com.googlecode.lanterna.TerminalFacade;
 import com.googlecode.lanterna.gui.GUIScreen;
 import org.CS2336.btree.BinaryTree;
+import org.CS2336.btree.Util;
 import org.CS2336.gfx.MainWindow;
+
+import java.io.File;
+import java.io.IOException;
 
 public class TreeSearch {
     public static BinaryTree<String> myTree = null;
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
+        if(args.length == 1) {
+            System.out.println(args[0]);
+            Util.buildTree(new File(args[0]));
+            Util.runReport(myTree);
+            System.out.println(Util.explode(myTree));
+            return;
+        }
+
         if(System.console() != null) {
             //draw inline iff a console is available
             System.setProperty("java.awt.headless", "true");
